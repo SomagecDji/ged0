59667e9d69a6177fb9eeaecf52e1ede3911ccb66
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-06-30 11:01:45 +0100

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -5,5 +5,6 @@ class DocumentFolder(models.Model):
     _description = 'Document folder'
     _inherit = 'documents.folder' 
     admin_group_ids = fields.Many2many('res.groups',  'documents_folder_admin_groups',string="Groupe d'écriture")
+    
 
 
